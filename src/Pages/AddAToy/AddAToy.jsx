018df2d9ab56452ca8200c4084723e3c640c8615
RabeyaAkter78 <rabeyariya251@gmail.com>
2023-05-21 11:21:19 +0600

    added add toy page

--- src/Pages/AddAToy/AddAToy.jsx
+++ src/Pages/AddAToy/AddAToy.jsx
@@ -1,9 +1,122 @@
-import React from 'react';
+import { useContext } from "react";
+import useTitle from "../../Routes/useTitle";
+import { AuthContext } from "../../Providers/AuthProviders";
+import toast, { Toaster } from 'react-hot-toast';
 
 const AddAToy = () => {
+    const { user } = useContext(AuthContext);
+    useTitle('Add-Toy')
+
+    const handleAddToys = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const sellerName = form.sellerName.value;
+        const photo = form.photo.value;
+        const email = form.email.value;
+        const category = form.category.value;
+        const price = form.price.value;
+        const quantity = form.quantity.value;
+        const ratings = form.ratings.value;
+        const description = form.description.value;
+
+        const addToy = { name, sellerName, email, photo, category, price, quantity, ratings, description };
+
+        fetch("http://localhost:5000/addAToy", {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(addToy)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+
+                if (data.insertedId) {
+                    toast('Added Successfully!');
+                }
+                form.reset();
+            })
+        console.log(addToy);
+    }
+    const handleToast = () => {
+    }
+
     return (
         <div>
-            <h2>add a toys</h2>
+
+            <form onSubmit={handleAddToys} className=' mb-12'>
+                <div className=" w-full bg-base-200">
+                    <h2 className='text-center text-blue-700 font-bold text-4xl'>Add Toy</h2>
+
+                    <div className="grid grid-cols-2 md:grid-cols-4 gap-4 p-4 w-full  shadow-2xl bg-base-100">
+
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Toy Name</span>
+                            </label>
+                            <input type="text" name='name' placeholder="Toy Name" className="input input-bordered" required />
+                        </div>
+
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Seller Name</span>
+                            </label>
+                            <input type="text" name="sellerName" placeholder="Seller Name" className="input input-bordered" defaultValue={user?.displayName} required />
+                        </div>
+
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" name="email" defaultValue={user?.email}
+                                placeholder="Email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Photo Url</span>
+                            </label>
+                            <input type="text" name='photo' placeholder="Photo Url" className="input input-bordered" />
+                        </div>
+                        <div className="form-control ">
+                            <label className="label">
+                                <span className="label-text">Category</span>
+                            </label>
+
+                            <input type="text" name='category' placeholder="Toy Category" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Price</span>
+                            </label>
+                            <input type="text" name='price' placeholder="Price" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Quantity</span>
+                            </label>
+                            <input type="text" name='quantity' placeholder="Quantity" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Ratings</span>
+                            </label>
+                            <input type="text" name='ratings' placeholder="Ratings" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control mb-4">
+                            <label className="label">
+                                <span className="label-text">Details Description</span>
+                            </label>
+                            <input type="text" name='description' placeholder="Details Description" className="input input-bordered" required />
+                        </div>
+                    </div>
+                    <div className="form-control mt-6">
+                        <button onClick={handleToast} className="btn btn-primary">Add</button>
+                        <Toaster />
+                    </div>
+                </div>
+            </form>
         </div>
     );
 };
--- src/Pages/Home/CategoryToys/CategoryToys.jsx
+++ src/Pages/Home/CategoryToys/CategoryToys.jsx
@@ -1,13 +1,98 @@
-import {Link} from 'react-router-dom'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import car7 from '../../../assets/images/gallery/car7.png'
+import car8 from '../../../assets/images/gallery/car8.png'
+import car9 from '../../../assets/images/gallery/car9.png'
+import { useEffect, useState } from 'react';
+
 const CategoryToys = () => {
+    const [toys, settoys] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/kidollsDetails')
+            .then(res => res.json())
+            .then(data => settoys(data))
+
+
+    }, [])
+
+
+
     return (
-        <div className="dropdown dropdown-hover  my-4 flex justify-center items-center">
-            <label tabIndex={0} className="text-6xl text-pink-700 font-bold ">Shop By Category</label>
-            <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
-                <li><Link>sports car</Link></li>
-                <li><Link> regular car</Link></li>
-                <li><Link> mini police car</Link></li>
-            </ul>
+        <div>
+{/* {
+    toys.map(toy=>)
+} */}
+
+
+            <div>
+                <div>
+                    <h2 className='text-3xl text-[#fb6e50] font-bold text-center mt-4  mb-5'>Chose Your Desired Cars</h2>
+                </div>
+                <Tabs className='text-center'>
+                    <TabList >
+                        <Tab >
+                            <div className="tooltip" data-tip="Sports Car">
+                                <img src={car7} alt="" />
+
+                            </div>
+                        </Tab>
+                        <Tab >
+                            <div className="tooltip" data-tip="Truck">
+                                <img src={car8} alt="" />
+                            </div>
+                        </Tab>
+                        <Tab >
+                            <div className="tooltip" data-tip="Regular Car">
+                                <img src={car9} alt="" />
+                            </div>
+                        </Tab>
+                    </TabList>
+
+                    <TabPanel>
+                        <div className="card w-96 bg-base-100 shadow-xl">
+                            <figure className="px-10 pt-10">
+                                <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+                            </figure>
+                            <div className="card-body items-center text-center">
+                                <h2 className="card-title">Shoes!</h2>
+                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                <div className="card-actions">
+                                    <button className="btn btn-primary">Buy Now</button>
+                                </div>
+                            </div>
+                        </div>
+                    </TabPanel>
+                    <TabPanel>
+                        <div className="card w-96 bg-base-100 shadow-xl">
+                            <figure className="px-10 pt-10">
+                                <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+                            </figure>
+                            <div className="card-body items-center text-center">
+                                <h2 className="card-title">Shoes!</h2>
+                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                <div className="card-actions">
+                                    <button className="btn btn-primary">Buy Now</button>
+                                </div>
+                            </div>
+                        </div>
+                    </TabPanel>
+                    <TabPanel>
+                        <div className="card w-96 bg-base-100 shadow-xl">
+                            <figure className="px-10 pt-10">
+                                <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+                            </figure>
+                            <div className="card-body items-center text-center">
+                                <h2 className="card-title">Shoes!</h2>
+                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                <div className="card-actions">
+                                    <button className="btn btn-primary">Buy Now</button>
+                                </div>
+                            </div>
+                        </div>
+                    </TabPanel>
+                </Tabs>
+            </div>
         </div>
     );
 };
