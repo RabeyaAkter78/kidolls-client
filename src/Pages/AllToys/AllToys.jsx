40d308238bce98a77ff55b447e8b3cdd15ab5b99
RabeyaAkter78 <rabeyariya251@gmail.com>
2023-05-23 14:09:21 +0600

    added implement search

--- src/Pages/AllToys/AllToys.jsx
+++ src/Pages/AllToys/AllToys.jsx
@@ -1,56 +1,49 @@
 import useTitle from "../../Routes/useTitle";
-import { Link, useLoaderData } from 'react-router-dom';
+import { Link} from 'react-router-dom';
 import { FaSearch } from 'react-icons/fa'
-import {  useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const AllToys = () => {
     const [search, setSearch] = useState("");
-    const allToys = useLoaderData();
+    const [allToys, setAllToys] = useState([]);
     useTitle('All-Toys');
 
     useEffect(() => {
-        fetch(`http://localhost:5000/searchToys/${search}`)
-                .then(res => res.json())
-                .then(data => {
-                    setSearch(data);
-                });
-        
+        fetch(`http://localhost:5000/allToys`)
+            .then(res => res.json())
+            .then(data => {
+                setAllToys(data)
+            })
+    }, [])
 
-    },[search])
-
+    const handleSeeAll = () => {
+        fetch(`http://localhost:5000/allToys`)
+            .then(res => res.json())
+            .then(data => {
+                setAllToys(data)
+            })
+    }
 
     const handleSearch = () => {
-       console.log(search)
+        fetch(`http://localhost:5000/searchToys/${search}`)
+            .then(res => res.json())
+            .then(data => {
+                setAllToys(data);
+            });
     }
-    // const handleSearch = () => {
-    //     fetch(`http://localhost:5000/searchToys/${search}`)
-    //         .then(res => res.json())
-    //         .then(data => {
-    //             console.log(data);
-    //         });
-    // }
-
-
-
-
-
 
     return (
         <div>
             {/* <h2 className="text-center text-2xl text-[#e05637] font-bold">All Toys</h2> */}
             <div className="flex justify-center items-center mt-8 mb-8">
-
                 <input onChange={(e) => setSearch(e.target.value)}
                     type="text"
                     placeholder="Type here"
-                    className="input input-bordered input-primary w-full max-w-md " />
-
-                <button onClick={handleSearch} className="btn btn-primary"> <FaSearch /></button>
+                    className="input input-bordered input-primary w-full max-w-md " />{""}
+                <button onClick={handleSearch} className="btn btn-info"> <FaSearch /></button>
+                <button onClick={handleSeeAll} className="btn btn-primary">See All</button>
             </div>
             <div className="overflow-x-auto ">
-
-
-
                 <table className="table w-full ">
                     <thead>
                         <tr>
@@ -83,26 +76,11 @@ const AllToys = () => {
                                             </div>
                                         </div>
                                     </td>
-                                    {/* <div className="flex flex-col font-bold ">
-                                <td><img style={{ height: '30px' }} src={toy.photo} alt="" /></td>
-                                <td>Seller:{toy.sellerName}</td>
-
-                            </div> */}
                                     <td className="font-bold">$: {toy.price}</td>
                                     <td className="font-bold">{toy.category}</td>
                                     <td className="font-bold">{toy.quantity}</td>
                                     <td><button className="btn btn-primary"><Link to="/singleToyDetails">Details</Link></button></td>
-
                                 </tr>
-
-
-
-                                // <Toys
-                                //     key={toy._id}
-                                //     toy={toy}
-                                //     index={index}
-                                // ></Toys>
-
                             )
                         }
                     </tbody>
